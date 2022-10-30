f5d252a6635141962c367c3768189976c7c9b9a5
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2022-10-29 22:23:23 -0700

    js: fix bugs/indent

--- js/Control.FullScreen.js
+++ js/Control.FullScreen.js
@@ -1,92 +1,98 @@
 L.Control.FullScreen = L.Control.extend({
-	options: {
-		position: 'topleft',
-		title: 'Full Screen',
-		forceSeparateButton: false
-	},
+  options: {
+    position: 'topleft',
+    title: 'Full Screen',
+    forceSeparateButton: false,
+  },
 
-	onAdd: function (map) {
-		// Do nothing if we can't
-		if (!fullScreenApi.supportsFullScreen)
-			return map.zoomControl._container;
+  onAdd: function(map) {
+    // Do nothing if we can't
+    if (!fullScreenApi.supportsFullScreen) {
+      return map.zoomControl._container;
+    }
 
-		var containerClass = 'leaflet-control-zoom', className, container;
+    const containerClass = 'leaflet-control-zoom'; let className; let container;
 
-		if(map.zoomControl && !this.options.forceSeparateButton) {
-			container = map.zoomControl._container;
-			className = '-fullscreen leaflet-bar-part leaflet-bar-part-bottom last';
-			// Update class of the zoom out button (Leaflet v0.5)
-			if (map.zoomControl._zoomOutButton) {
-				L.DomUtil.removeClass(map.zoomControl._zoomOutButton, 'leaflet-bar-part-bottom');
-			}
-		} else {
-			container = L.DomUtil.create('div', containerClass);
-			className = '-fullscreen leaflet-bar leaflet-bar-part last';
-		}
+    if (map.zoomControl && !this.options.forceSeparateButton) {
+      container = map.zoomControl._container;
+      className = '-fullscreen leaflet-bar-part leaflet-bar-part-bottom last';
+      // Update class of the zoom out button (Leaflet v0.5)
+      if (map.zoomControl._zoomOutButton) {
+        L.DomUtil.removeClass(map.zoomControl._zoomOutButton,
+            'leaflet-bar-part-bottom');
+      }
+    } else {
+      container = L.DomUtil.create('div', containerClass);
+      className = '-fullscreen leaflet-bar leaflet-bar-part last';
+    }
 
-		this._createButton(this.options.title, containerClass + className, container, this.toogleFullScreen, map);
+    this._createButton(this.options.title,
+        containerClass + className,
+        container,
+        this.toogleFullScreen,
+        map);
 
-		return container;
-	},
+    return container;
+  },
 
-	_createButton: function (title, className, container, fn, context) {
-		var link = L.DomUtil.create('a', className, container);
-		link.href = '#';
-		link.title = title;
+  _createButton: function(title, className, container, fn, context) {
+    const link = L.DomUtil.create('a', className, container);
+    link.href = '#';
+    link.title = title;
 
-		L.DomEvent
-			.addListener(link, 'click', L.DomEvent.stopPropagation)
-			.addListener(link, 'click', L.DomEvent.preventDefault)
-			.addListener(link, 'click', fn, context);
+    L.DomEvent
+        .addListener(link, 'click', L.DomEvent.stopPropagation)
+        .addListener(link, 'click', L.DomEvent.preventDefault)
+        .addListener(link, 'click', fn, context);
 
-		L.DomEvent
-			.addListener(container, fullScreenApi.fullScreenEventName, L.DomEvent.stopPropagation)
-			.addListener(container, fullScreenApi.fullScreenEventName, L.DomEvent.preventDefault)
-			.addListener(container, fullScreenApi.fullScreenEventName, this._handleEscKey, context);
+    const fsEventName = fullScreenApi.fullScreenEventName;
+    L.DomEvent
+        .addListener(container, fsEventName, L.DomEvent.stopPropagation)
+        .addListener(container, fsEventName, L.DomEvent.preventDefault)
+        .addListener(container, fsEventName, this._handleEscKey, context);
 
-		L.DomEvent
-			.addListener(document, fullScreenApi.fullScreenEventName, L.DomEvent.stopPropagation)
-			.addListener(document, fullScreenApi.fullScreenEventName, L.DomEvent.preventDefault)
-			.addListener(document, fullScreenApi.fullScreenEventName, this._handleEscKey, context);
+    L.DomEvent
+        .addListener(document, fsEventName, L.DomEvent.stopPropagation)
+        .addListener(document, fsEventName, L.DomEvent.preventDefault)
+        .addListener(document, fsEventName, this._handleEscKey, context);
 
-		return link;
-	},
+    return link;
+  },
 
-	toogleFullScreen: function () {
-		this._exitFired = false;
-		if (fullScreenApi.supportsFullScreen){
-			var container = this._container;
-			if(fullScreenApi.isFullScreen(container)){
-				fullScreenApi.cancelFullScreen(container);
-				this.invalidateSize();
-				this.fire('exitFullscreen');
-				this._exitFired = true;
-			}
-			else {
-				fullScreenApi.requestFullScreen(container);
-				this.invalidateSize();
-				this.fire('enterFullscreen');
-			}
-		}
-	},
+  toogleFullScreen: function() {
+    this._exitFired = false;
+    if (fullScreenApi.supportsFullScreen) {
+      const container = this._container;
+      if (fullScreenApi.isFullScreen(container)) {
+        fullScreenApi.cancelFullScreen(container);
+        this.invalidateSize();
+        this.fire('exitFullscreen');
+        this._exitFired = true;
+      } else {
+        fullScreenApi.requestFullScreen(container);
+        this.invalidateSize();
+        this.fire('enterFullscreen');
+      }
+    }
+  },
 
-	_handleEscKey: function () {
-		if(!fullScreenApi.isFullScreen(this) && !this._exitFired){
-			this.fire('exitFullscreen');
-			this._exitFired = true;
-		}
-	}
+  _handleEscKey: function() {
+    if (!fullScreenApi.isFullScreen(this) && !this._exitFired) {
+      this.fire('exitFullscreen');
+      this._exitFired = true;
+    }
+  },
 });
 
-L.Map.addInitHook(function () {
-	if (this.options.fullscreenControl) {
-		this.fullscreenControl = L.control.fullscreen(this.options.fullscreenControlOptions);
-		this.addControl(this.fullscreenControl);
-	}
+L.Map.addInitHook(function() {
+  if (this.options.fullscreenControl) {
+    this.fullscreenControl = L.control.fullscreen(this.options.fullscreenControlOptions);
+    this.addControl(this.fullscreenControl);
+  }
 });
 
-L.control.fullscreen = function (options) {
-	return new L.Control.FullScreen(options);
+L.control.fullscreen = function(options) {
+  return new L.Control.FullScreen(options);
 };
 
 /*
@@ -99,68 +105,69 @@ source : http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugi
 */
 
 (function() {
-	var
-		fullScreenApi = {
-			supportsFullScreen: false,
-			isFullScreen: function() { return false; },
-			requestFullScreen: function() {},
-			cancelFullScreen: function() {},
-			fullScreenEventName: '',
-			prefix: ''
-		},
-		browserPrefixes = 'webkit moz o ms khtml'.split(' ');
+  const
+    fullScreenApi = {
+      supportsFullScreen: false,
+      isFullScreen: function() {
+        return false;
+      },
+      requestFullScreen: function() {},
+      cancelFullScreen: function() {},
+      fullScreenEventName: '',
+      prefix: '',
+    };
+  const browserPrefixes = 'webkit moz o ms khtml'.split(' ');
 
-	// check for native support
-	if (typeof document.exitFullscreen != 'undefined') {
-		fullScreenApi.supportsFullScreen = true;
-	} else {
-		// check for fullscreen support by vendor prefix
-		for (var i = 0, il = browserPrefixes.length; i < il; i++ ) {
-			fullScreenApi.prefix = browserPrefixes[i];
+  // check for native support
+  if (typeof document.exitFullscreen != 'undefined') {
+    fullScreenApi.supportsFullScreen = true;
+  } else {
+    // check for fullscreen support by vendor prefix
+    for (let i = 0, il = browserPrefixes.length; i < il; i++ ) {
+      fullScreenApi.prefix = browserPrefixes[i];
 
-			if (typeof document[fullScreenApi.prefix + 'CancelFullScreen' ] != 'undefined' ) {
-				fullScreenApi.supportsFullScreen = true;
+      if (typeof document[fullScreenApi.prefix + 'CancelFullScreen'] != 'undefined' ) {
+        fullScreenApi.supportsFullScreen = true;
 
-				break;
-			}
-		}
-	}
+        break;
+      }
+    }
+  }
 
-	// update methods to do something useful
-	if (fullScreenApi.supportsFullScreen) {
-		fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
+  // update methods to do something useful
+  if (fullScreenApi.supportsFullScreen) {
+    fullScreenApi.fullScreenEventName = fullScreenApi.prefix + 'fullscreenchange';
 
-		fullScreenApi.isFullScreen = function() {
-			switch (this.prefix) {
-				case '':
-					return document.fullScreen;
-				case 'webkit':
-					return document.webkitIsFullScreen;
-				default:
-					return document[this.prefix + 'FullScreen'];
-			}
-		}
-		fullScreenApi.requestFullScreen = function(el) {
-			return (this.prefix === '') ? el.requestFullscreen() : el[this.prefix + 'RequestFullScreen']();
-		}
-		fullScreenApi.cancelFullScreen = function(el) {
-			return (this.prefix === '') ? document.exitFullscreen() : document[this.prefix + 'CancelFullScreen']();
-		}
-	}
+    fullScreenApi.isFullScreen = function() {
+      switch (this.prefix) {
+        case '':
+          return document.fullScreen;
+        case 'webkit':
+          return document.webkitIsFullScreen;
+        default:
+          return document[this.prefix + 'FullScreen'];
+      }
+    };
+    fullScreenApi.requestFullScreen = function(el) {
+      return (this.prefix === '') ? el.requestFullscreen() : el[this.prefix + 'RequestFullScreen']();
+    };
+    fullScreenApi.cancelFullScreen = function(el) {
+      return (this.prefix === '') ? document.exitFullscreen() : document[this.prefix + 'CancelFullScreen']();
+    };
+  }
 
-	// jQuery plugin
-	if (typeof jQuery != 'undefined') {
-		jQuery.fn.requestFullScreen = function() {
+  // jQuery plugin
+  if (typeof jQuery != 'undefined') {
+    jQuery.fn.requestFullScreen = function() {
+      return this.each(function() {
+        const el = jQuery(this);
+        if (fullScreenApi.supportsFullScreen) {
+          fullScreenApi.requestFullScreen(el);
+        }
+      });
+    };
+  }
 
-			return this.each(function() {
-				var el = jQuery(this);
-				if (fullScreenApi.supportsFullScreen) {
-					fullScreenApi.requestFullScreen(el);
-				}
-			});
-		};
-	}
-
-	// export api
-	window.fullScreenApi = fullScreenApi;
+  // export api
+  window.fullScreenApi = fullScreenApi;
 })();
--- js/maps.js
+++ js/maps.js
@@ -1,54 +1,54 @@
-var map;
+let map;
 
 function show(feature, layer) {
-    return feature.properties.show_on_map;
+  return feature.properties.show_on_map;
 }
 
 function addTooltip(feature, layer) {
-    if (feature.properties && feature.properties.name) {
-        layer.bindTooltip('<b>'+feature.properties.name+'</b>');
-    }
+  if (feature.properties && feature.properties.name) {
+    layer.bindTooltip('<b>'+feature.properties.name+'</b>');
+  }
 }
 
 function markerBounce(event) {
-    event.target.bounce({height: 50});
+  event.target.bounce({height: 50});
 }
 
 function addClickBounce(feature, latlng) {
-     marker = new L.Marker(latlng, {bounceOnAdd: true}).addTo(map);
-     marker.on('click', markerBounce);
-     return marker;
+  marker = new L.Marker(latlng, {bounceOnAdd: true}).addTo(map);
+  marker.on('click', markerBounce);
+  return marker;
 }
 
 function addFeature(feature) {
-   L.geoJson(feature, {
-	filter: show,
-        onEachFeature: addTooltip,
-        pointToLayer: addClickBounce,
-   }).addTo(map);
+  L.geoJson(feature, {
+    filter: show,
+    onEachFeature: addTooltip,
+    pointToLayer: addClickBounce,
+  }).addTo(map);
 }
 
 function addData() {
-    $.getJSON('places.geojson', function(data) {
-        let i = 0;
-        var add_feat = setInterval(function() {
-            let feat = data.features[i++];
-            addFeature(feat);
-            if (i >= data.features.length) clearInterval(add_feat);
-        }, 30);
-    });
+  $.getJSON('places.geojson', function(data) {
+    let i = 0;
+    const addFeat = setInterval(function() {
+      const feat = data.features[i++];
+      addFeature(feat);
+      if (i >= data.features.length) clearInterval(addFeat);
+    }, 30);
+  });
 }
 
 window.onload = function() {
-    map = L.map('map').setView([23.26, 0], 3);
-    L.control.fullscreen({
-        position: 'topleft',
-        title: 'Go Fullscreen!',
-    }).addTo(map);
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-         maxZoom: 19,
-         subdomains: ['a', 'b', 'c'],
-	 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-    map.whenReady(addData);
+  map = L.map('map').setView([23.26, 0], 3);
+  L.control.fullscreen({
+    position: 'topleft',
+    title: 'Go Fullscreen!',
+  }).addTo(map);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    subdomains: ['a', 'b', 'c'],
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+  map.whenReady(addData);
 };
