1726ab0909df73af71fbb70ecb1f1a06a7aad2c6
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2022-10-29 22:25:29 -0700

    maps.js: update fix/indent

--- js/maps.js
+++ js/maps.js
@@ -1,32 +1,32 @@
-var map;
+let map;
 
 function addTooltip(feature, layer) {
-    if (feature.properties && feature.properties.name) {
-        layer.bindTooltip('<b>'+feature.properties.name+'</b>')
-    }
+  if (feature.properties && feature.properties.name) {
+    layer.bindTooltip('<b>'+feature.properties.name+'</b>');
+  }
 }
 
-function show(feature, layer){
-    return feature.properties.show_on_map;
+function show(feature, layer) {
+  return feature.properties.show_on_map;
 }
 
-function addFeature(feature){
-   L.geoJson(feature, {
-        onEachFeature: addTooltip,
-        filter: show,
-    }).addTo(map);
+function addFeature(feature) {
+  L.geoJson(feature, {
+    onEachFeature: addTooltip,
+    filter: show,
+  }).addTo(map);
 }
 window.onload = function() {
-    map = L.map('map').setView([23.26, 0], 3);
-    L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-         maxZoom: 19,
-         subdomains: ["a", "b", "c"],
-	 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+  map = L.map('map').setView([23.26, 0], 3);
+  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    subdomains: ['a', 'b', 'c'],
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
 
-   $.getJSON('places.geojson', function(data) {
-        $.each(data.features, function(i, feat) {
-            addFeature(feat);
-        });
+  $.getJSON('places.geojson', function(data) {
+    $.each(data.features, function(i, feat) {
+      addFeature(feat);
     });
-}
+  });
+};
